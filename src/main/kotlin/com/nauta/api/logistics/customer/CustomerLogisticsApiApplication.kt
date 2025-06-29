e9e8f16b89f2ef37d3e317c5952e08b1bda420c0
claudio <claudiodaniel.carrizo93@gmail.com>
2025-06-29 13:42:08 -0300

    [project-structure]: Add base project configuration for api microservice

---
+++ src/main/kotlin/com/nauta/api/logistics/customer/CustomerLogisticsApiApplication.kt
@@ -0,0 +1,11 @@
+package com.nauta.api.logistics.customer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomerLogisticsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CustomerLogisticsApiApplication>(*args)
+}
